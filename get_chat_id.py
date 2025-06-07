6fa10583c44ef4213725e205fc1e5d65dce78d8b
paologio007 <giorgierip@gmail.com>
2025-06-07 12:07:59 +0200

    Delete get_chat_id.py

--- get_chat_id.py
+++
@@ -1,8 +0,0 @@
-import requests
-import os
-
-TOKEN = os.environ['TELEGRAM_BOT_TOKEN']
-URL = f"https://api.telegram.org/bot{TOKEN}/getUpdates"
-
-response = requests.get(URL)
-print(response.json())
